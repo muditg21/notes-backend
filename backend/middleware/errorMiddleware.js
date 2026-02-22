01a6082ae7633a7160a6ec8725de48b5b959f5db
muditg21 <muditgupta340@gmail.com>
2026-02-23 00:40:54 +0530

    added castError

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,5 +1,11 @@
 const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message=err.message;
+
+  if(err.name === "CastError"){
+    statusCode=400;
+    message="Invalid ID format";  
+  }
 
   res.status(statusCode).json({
     message: err.message,
